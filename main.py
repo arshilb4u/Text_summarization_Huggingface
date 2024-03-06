d8abc183fa0c438e5a63dc2123782445d101a9ef
arshil <arshilsingh711@gmail.com>
2024-03-06 17:18:32 +0530

    Add data Ingestion pipeline and init main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from textsummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarization.logging import logger
 
-logger.info("Welcome to custom model")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx=========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
