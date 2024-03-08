bba53f1b3d60b6e8370ad8f3e014252285785e6a
arshil <arshilsingh711@gmail.com>
2024-03-08 09:49:13 +0530

    added data validation pipeline

--- src/textsummarization/config/configuration/__init__.py
+++ src/textsummarization/config/configuration/__init__.py
@@ -1,7 +1,7 @@
 import textsummarization
 from textsummarization.constants import *
 from textsummarization.utils.common import read_yaml, create_directories
-from textsummarization.entity import DataIngestionConfig
+from textsummarization.entity import DataIngestionConfig,DataValidationConfig
 
 
 class ConfigurationManager:
@@ -30,3 +30,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
